f91a523ad435838dd7db8bff382c38f3e6bad33f
Shaginur <shaginu@gmail.com>
2024-03-28 16:04:22 +0500

    [Memory-game]-001 final

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,12 @@
 const EMOJI = ["🥔", "🍒", "🥑", "🌽", "🥕", "🍇", "🍉", "🍌", "🥭", "🍍"];
+let cardContent = [];
 
 //функция перемешивает изначальный масив получает первые восемь элементов создает пары и снова перемешивает их
 function shuffle(EMOJI) {
   const shaffleEmoji = EMOJI.sort(() => Math.random(EMOJI) - 0.5);
   const receivingEmoji = shaffleEmoji.slice(0, 8);
   const rezalt = [...receivingEmoji, ...receivingEmoji].sort(
-    () => Math.random([...receivingEmoji, ...receivingEmoji]) - 0,
-    5
-  );
+    () => Math.random([...receivingEmoji, ...receivingEmoji]) - 0.5);
   return rezalt;
 }
 
@@ -27,6 +26,7 @@ const STATE = {
 };
 
 /**
+ * @property {HTMLDivElement} win - текст победы.
  * @property {HTMLDivElement} boardContainer - Контейнер игрового поля.
  * @property {HTMLDivElement} board - Основное содержимое поля (4x4).
  * @property {HTMLDivElement} moves - Контрол для учета шагов.
@@ -57,51 +57,58 @@ function Render() {
 
 //функция переворота карточек
 function flippCards() {
-
   const cards = SELECTORS.board.children;
 
   [...cards].forEach((element) => {
-
     element.addEventListener("click", (event) => {
-
       element.classList.add("flipped");
+      cardContent.push(event.target.nextSibling.nextSibling);
 
-      STATE.moves++
-      SELECTORS.moves.innerHTML = `${STATE.moves} шагов`
+      STATE.flipp++;
+      STATE.moves++;
+      SELECTORS.moves.innerHTML = `${STATE.moves} шагов`;
+      if (cardContent.length === 2) ifCardСomparison(cardContent);
     });
   });
 }
 
-
-//функция возврата карточек удалением класса
-function flipBackCards() {
-
- const cardBack = document.querySelectorAll('.card')
- cardBack.forEach((item) => {
-  if(item.classList.contains('flipped')) {
-    item.classList.remove('flipped')
-
+//функция проверяет карточки удвляет класс при не совпадении при открытие всех пар выводит текст победы
+function ifCardСomparison(item) {
+  if (item[0].textContent === item[1].textContent) {
+    cardContent = []
+    STATE.totalFlipp += 1;
+    if(STATE.totalFlipp === 8) {
+      document.querySelector('.win').innerHTML = `
+      <h3>Вы выиграли!</h3>
+      <span>Время  ${STATE.time}сек</span>
+      <span>количество шагов ${STATE.moves}</span>`
+      SELECTORS.boardContainer.classList.add('flipped')
+    }
+  }else{
+    setTimeout(
+      () => item[0].parentElement.classList.remove("flipped"),
+      1000
+    );
+    setTimeout(
+      () => item[1].parentElement.classList.remove("flipped"),
+      1000
+    );
+   cardContent = [], 1000;
   }
- })
+  
 }
 
-flipBackCards()
+
 //функция начала игры вызывает другие функции проверяет условия
-SELECTORS.start.addEventListener('click', () => {
-  timerSetInterval()
-  flippCards()
+SELECTORS.start.addEventListener("click", () => {
+  flippCards();
+  setInterval(timerPlus, 1000);
+  
+});
 
-
-
-})
-
-//функция вызывает timerPlus() казждую секунду
-function timerSetInterval() {
-   setInterval(timerPlus, 1000)
- }
-
- //функция что увеличевает значение STATE.time на один
- function timerPlus() {
-  STATE.time++
+//функция что увеличевает значение STATE.time на один
+function timerPlus() {
+  STATE.time++;
   SELECTORS.timer.innerHTML = `время: ${STATE.time} sec`;
- }
+}
+
